674809bbe4256b1d01b534c22f40eb7a7e59f71e
EurasianChaffinch <EurasianChaffinch@proton.me>
2026-02-09 14:51:31 +0200

    Update index.js

--- express-app/index.js
+++ express-app/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>If you see it - it works!/h1>')
+  res.send('<h1>If you see it - it works!</h1>')
 })
 
 const PORT = 8080
