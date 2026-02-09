7807fa6b102a4a8fae262800de13acb686486370
EurasianChaffinch <EurasianChaffinch@proton.me>
2026-02-09 11:21:41 +0200

    Update index.js

--- express-app/index.js
+++ express-app/index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
-#throw new Error("Test failure - intentional crash for rollback testing");
+
 app.get('/', (req, res) => {
-  res.send('<h1>Test</h1>')
+  res.send('<h1>Check</h1>')
 })
 
 const PORT = 8080
